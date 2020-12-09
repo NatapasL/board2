8f83a83cd722d4dcde5e1c5b17c3d363e2b5ac2c
Natapas <natapas.lue@gmail.com>
2020-12-10 02:12:59 +0700

    Add hook useStore

---
+++ src/hooks/useStore.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+
+import { storesContext } from 'src/contexts/storesContext'
+
+export const useStore = () => {
+  const context = useContext(storesContext)
+
+  return context
+}
