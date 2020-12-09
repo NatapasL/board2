0390f250ef775fbc448114edef97b51e9a2fd59d
Natapas <natapas.lue@gmail.com>
2020-12-10 05:17:38 +0700

    Remove text on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,7 @@ import axios from 'axios'
 import { Layout } from '../components/Layout'
 
 export default () => {
-  return <Layout>index</Layout>
+  return (
+    <Layout></Layout>
+  )
 }
-
-// export const getServerSideProps = async () => {
-//   axios.post('http://localhost:3000/api/get', {
-//     path: 'boards',
-//   }).then(({ data }) => console.log(data))
-
-//   return { props: {} }
-// }
