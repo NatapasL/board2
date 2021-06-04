d4272217a1af1e3acac59f5ff6af047e180de75b
Natapas <natapas.lue@gmail.com>
2021-06-05 03:53:29 +0700

    Set local host to 0.0.0.0

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
   trailingSlash: true,
   env: {
-    HOST: process.env.HOST || 'http://localhost:3000',
+    HOST: process.env.HOST || 'http://0.0.0.0:3000',
     BACKEND_BASE_URL:
       process.env.BACKEND_BASE_URL ||
         'https://asia-southeast2-reader-app-fe4ab.cloudfunctions.net/fanboi-api'
