0d1ec01c7362128fd25d49e646eb14430f610828
Natapas Luebangyai <natapas.lue@gmail.com>
2023-02-25 16:13:30 +0700

    Add common api

---
+++ src/pages/api/common.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+import {
+  HANDLER_ALL_POSTS,
+  HANDLER_ALL_TOPICS, HANDLER_GET_TOPIC, HANDLER_RECENT_POSTS, HANDLER_RECENT_TOPICS
+} from 'src/constants/api';
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const handler = request.query.handler;
+  if (!handler) {
+    response.status(400).json({ message: `no handler query` });
+  }
+
+  switch(handler) {
+    case HANDLER_RECENT_TOPICS:
+      return axios.get(`https://fanboi.ch/api/1.0/boards/${request.query.board_slug}?topics=1`).then((res) => {
+        response.send(res.data);
+      });
+    case HANDLER_ALL_TOPICS:
+      return axios.get(`https://fanboi.ch/api/1.0/boards/${request.query.board_slug}/topics`).then((res) => {
+        response.send(res.data);
+      });
+    case HANDLER_GET_TOPIC:
+      return axios.get(`https://fanboi.ch/api/1.0/topics/${request.query.topic_id}`).then((res) => {
+        response.send(res.data);
+      });
+    case HANDLER_RECENT_POSTS:
+      return axios.get(`https://fanboi.ch/api/1.0/topics/${request.query.topic_id}?posts=1`).then((res) => {
+        response.send(res.data);
+      });
+    case HANDLER_ALL_POSTS:
+      return axios.get(`https://fanboi.ch/api/1.0/topics/${request.query.topic_id}/posts`).then((res) => {
+        response.send(res.data);
+      });
+  }
+
+}
