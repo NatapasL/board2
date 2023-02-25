c65ca20c000c7b7ae7afe758e902255e8e7d8313
Natapas Luebangyai <natapas.lue@gmail.com>
2023-02-25 16:13:10 +0700

    Set BACKEND_BASE_URL

--- next.config.js
+++ next.config.js
@@ -13,6 +13,6 @@ module.exports = {
     HOST: process.env.HOST || 'http://0.0.0.0:3000',
     BACKEND_BASE_URL:
       process.env.BACKEND_BASE_URL ||
-        'https://asia-southeast2-reader-app-fe4ab.cloudfunctions.net/fanboi-api'
+        '/api/common'
   },
 }
