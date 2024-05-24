dfd022f4449faea62b68210ffc62d272cc8fc52e
NatapasL <natapas.lue@gmail.com>
2024-05-24 17:58:57 +0700

    Change api service to ts

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-import { HANDLER_RECENT_POSTS, HANDLER_ALL_POSTS } from 'src/constants/api';
+import { HANDLER_ALL_POSTS, HANDLER_RECENT_POSTS } from 'src/constants/api';
 
-export const getRecentPosts = (topicId) => {
+export const getRecentPosts = (topicId: string) => {
   const query = `handler=${HANDLER_RECENT_POSTS}&topic_id=${topicId}`;
 
   return axios.get(`${process.env.BACKEND_BASE_URL}?${query}`);
 };
 
-export const getAllPosts = (topicId) => {
+export const getAllPosts = (topicId: string) => {
   const query = `handler=${HANDLER_ALL_POSTS}&topic_id=${topicId}`;
 
   return axios.get(`${process.env.BACKEND_BASE_URL}?${query}`);
--- src/api/topicApi.ts
+++ src/api/topicApi.ts
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 import {
+  HANDLER_ALL_TOPICS,
   HANDLER_GET_TOPIC,
   HANDLER_RECENT_TOPICS,
-  HANDLER_ALL_TOPICS,
-} from './../constants/api';
+} from '../constants/api';
 
-export const getTopic = (topicId) => {
+export const getTopic = (topicId: string) => {
   const query = `handler=${HANDLER_GET_TOPIC}&topic_id=${topicId}`;
 
   return axios.get(`${process.env.BACKEND_BASE_URL}?${query}`);
 };
 
-export const getRecentTopics = (boardSlug) => {
+export const getRecentTopics = (boardSlug: string) => {
   const query = `handler=${HANDLER_RECENT_TOPICS}&board_slug=${boardSlug}`;
 
   return axios.get(`${process.env.BACKEND_BASE_URL}?${query}`);
 };
 
-export const getAllTopics = (boardSlug) => {
+export const getAllTopics = (boardSlug: string) => {
   const query = `handler=${HANDLER_ALL_TOPICS}&board_slug=${boardSlug}`;
 
   return axios.get(`${process.env.BACKEND_BASE_URL}?${query}`);
