f629b5702a80fe71357dc92c1cd2b0db59e46e0b
NatapasL <natapas.lue@gmail.com>
2024-05-25 21:57:07 +0700

    Try call api directly

--- src/api/replyApi.ts
+++ src/api/replyApi.ts
@@ -5,5 +5,7 @@ import { PostReplyForm } from 'src/models';
 export const postReply = (topicId: string, data: PostReplyForm): Promise<AxiosResponse<any>> => {
   const query = `handler=${HANDLER_POST_REPLY}&topic_id=${topicId}`;
 
-  return axios.post(`${process.env.BACKEND_BASE_URL}?${query}`, { body: data.text, bumped: data.bumpTopic });
+  // return axios.post(`${process.env.BACKEND_BASE_URL}?${query}`, { body: data.text, bumped: data.bumpTopic });
+
+  return axios.post(`https://fanboi.ch/api/1.0/topics/${topicId}/posts/`, { body: data.text, bumped: data.bumpTopic });
 };
