8fd408a4d27249ef09b102dae119b2dc4de6530f
NatapasL <natapas.lue@gmail.com>
2024-08-02 13:39:51 +0700

    Update board model

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,16 +1,7 @@
 export interface Board {
-  type: string;
   id: number;
   description: string;
-  settings: {
-    name: string;
-    use_ident: boolean;
-    max_posts: number;
-    post_delay: number;
-    expire_duration: number;
-  };
   slug: string;
-  status: string;
   title: string;
   path: string;
 }
